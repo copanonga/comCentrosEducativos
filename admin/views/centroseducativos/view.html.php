555f93e338b4095133e898080ab224654178a7cf
Jose <josemsg@gmail.com>
2018-05-23 10:33:29 +0200

    Creada barra de herramientas en zona de administración

--- admin/views/centroseducativos/view.html.php
+++ admin/views/centroseducativos/view.html.php
@@ -6,11 +6,34 @@ class centroseducativosViewcentroseducativos extends JViewLegacy {
     
 	function display($tpl = null) {
             
-		$cabecera = "Centros educativos";
-		$this->assignRef ( 'cabecera', $cabecera );
-		
-		parent::display ( $tpl );
+            $cabecera = "Centros educativos";
+            $this->assignRef ( 'cabecera', $cabecera );
+
+            $this->crearBarraHerramientas();
+
+            parent::display ( $tpl );
                 
 	}
+        
+        function crearBarraHerramientas () {
+            
+            /*
+             * Configurar la barra de herramientas
+             */
+            JToolBarHelper::title(   JText::_( 'Centros educativos' ), 'generic.png' );
+
+            #function addNew($task = 'add', $alt = 'JTOOLBAR_NEW', $check = false)
+            JToolBarHelper::addNew($task = 'nuevo', $alt = 'Nuevo', $check = false);
+
+            #function editList($task = 'edit', $alt = 'JTOOLBAR_EDIT')
+            JToolBarHelper::editList($task = 'edit', $alt = 'Editar');
+
+            #function makeDefault($task = 'default', $alt = 'JTOOLBAR_DEFAULT')
+            JToolBarHelper::makeDefault ($task = 'predeterminado', $alt = 'Valor por defecto');
+
+            #function deleteList($msg = '', $task = 'remove', $alt = 'JTOOLBAR_DELETE')
+            JToolBarHelper::deleteList($msg = '', $task = 'remove', $alt = 'Borrar');
+            
+        }
         
 }
