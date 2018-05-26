7f46829decd619cc24f51072f2495810f5f168e0
Jose <josemsg@gmail.com>
2018-05-26 11:04:27 +0200

    Refactorizados modelos

--- admin/models/centroseducativos.php
+++ admin/models/centroseducativos.php
@@ -1,70 +1,56 @@
 <?php
 
+/*
+ *
+ *  Modelo
+ *  
+ *  
+ */
+
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 class centroseducativosModelcentroseducativos extends JModelLegacy
 {
-	/**
-	 *  @var array
-	 */
-	var $_data;
+ 
+    var $_data;
 
-	/**
-	 * devuelve la cadena con el comando que se usara para recuperar los datos
-	 */
-	function _buildQuery()
-	{
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
-			echo "------------------------- <br> ";
-			echo "estoy en .....:". __CLASS__." <br>";
-			echo "estoy en .....:". __METHOD__." <br>";
-			echo "------------------------- <br> ";
-		}
-		
-		$query = ' SELECT * '
-			     . ' FROM #__centroseducativos '
-		;
+    function _buildQuery()
+    {
+      
+        $this->mostrarZona(__CLASS__,__METHOD__);
 
-		return $query;
-	}
+        $query = ' SELECT * FROM #__centroseducativos';
 
-	/**
-	 * recupera los datos de la tabla Tablahola04 
-	 * devuelve un array de objetos conteniendo los datos
-	 */
-	function getData()
-	{
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
-			echo "------------------------- <br> ";
-			echo "estoy en .....:". __CLASS__." <br>";
-			echo "estoy en .....:". __METHOD__." <br>";
-			echo "------------------------- <br> ";
-		}
-		
-		// cargamos los datos si está vacio el array
-		if (empty( $this->_data ))
-		{
-			//recupera el comando a ejecutar
-			$query = $this->_buildQuery();
-			
-			//recupera los datos
-			$this->_data = $this->_getList( $query );
-		}
-
-		//devuelve los datos
-		return $this->_data;
-	}
+        return $query;
         
-        function getPatata()
-	{
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
-			echo "------------------------- <br> ";
-			echo "estoy en .....:". __CLASS__." <br>";
-			echo "estoy en .....:". __METHOD__." <br>";
-			echo "------------------------- <br> ";
-		}
-		
-		return "Devolver patata";
-	}
+    }
+
+    function getData()
+    {
+        
+        $this->mostrarZona(__CLASS__,__METHOD__);
+
+        if (empty( $this->_data ))
+        {
+                
+            $query = $this->_buildQuery();
+            $this->_data = $this->_getList( $query );
+            
+        }
+
+        return $this->_data;
+        
+    }
+    
+    function mostrarZona($clase,$metodo){
+        
+        if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
+                echo "------------------------- <br> ";
+                echo "Clase: ". $clase ." <br>";
+                echo "Método: ". $metodo ." <br>";
+                echo "------------------------- <br> ";
+        }
+        
+    }
         
 }
--- admin/models/centroseducativoscrud.php
+++ admin/models/centroseducativoscrud.php
@@ -1,141 +1,163 @@
 <?php
+
 /*
- * ********************************************************* 
- * package com_hola04
- * file: admin\models\hola04.php
- * ********************************************************* 
+ *
+ *  Modelo
+ *  
+ *  
  */
-defined( '_JEXEC' ) or die( 'Restricted access' );
+
+defined( '_JEXEC' ) or die( 'Acceso restringido' );
 
 class centroseducativosModelcentroseducativoscrud extends JModelLegacy
 {
-	/**
-	 * Constructor que recupera el ID de la cadena de peticion
-	 */
-	function __construct()
-	{
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-			echo "ejecutando la funcion __construct de centroseducativosModelcentroseducativosCRUD <BR>";
-		parent::__construct();
+    
+    /**
+     * Constructor que recupera el ID de la cadena de peticion
+     */
+    function __construct()
+    {
 
-		$array = JRequest::getVar('cid',  0, '', 'array');
-		$this->setId((int)$array[0]);
-	}
+        $this->mostrarZona(__CLASS__,__METHOD__);
         
-        /**
-	 * Metodo para establecer el identificador 
-	 *
-	 * @param	int centroseducativosModelcentroseducativosCRUD identificador
-	 * no devuelve nada
-	 */
-	function setId($id)
-	{
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-			echo "ejecutando la funcion setId con id=$id de centroseducativosModelcentroseducativosCRUD <BR>";
-		// Asigna el id y elimina datos
-		$this->_id		= $id;
-		$this->_data	= null;
-	}
+        parent::__construct();
+
+        $array = JRequest::getVar('cid',  0, '', 'array');
+        $this->setId((int)$array[0]);
+
+    }
         
-        /**
-	 * Metodo para obtener los datos
-	 * devuelve un objeto con los datos
-	 */
-	function &getData()
-	{
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-			echo "ejecutando la funcion getdata de centroseducativosModelcentroseducativosCRUD <BR>";
-		// carga los datos
-		if (empty( $this->_data )) {
-			$query = ' SELECT * FROM #__centroseducativos '.
-					'  WHERE id = '.$this->_id;
-			if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-				echo "\$query...: $query <BR>";
-			$this->_db->setQuery( $query );
-			$this->_data = $this->_db->loadObject();
-		}
-		if (!$this->_data) {
-			$this->_data = new stdClass();
-			$this->_data->id = 0;
-			$this->_data->nombre = null;
-		}
-		return $this->_data;
-	}
+    /**
+     *  Asigna el id y elimina datos
+     *
+     * @param	int centroseducativosModelcentroseducativosCRUD identificador
+     * no devuelve nada
+     */
+    function setId($id)
+    {
         
-        /**
-	 * Metodo para almacenar un registro
-	 * devuelve una variable booleana con valor true ha finalizado con exito
-	 */
-	function store()
-	{
+        $this->mostrarZona(__CLASS__,__METHOD__);
+        
+        $this->_id  = $id;
+        $this->_data = null;
+        
+    }
+        
+    /**
+     * Metodo para obtener los datos
+     * devuelve un objeto con los datos
+     */
+    function &getData()
+    {
+        
+        $this->mostrarZona(__CLASS__,__METHOD__);
+        
+        // carga los datos
+        if (empty( $this->_data )) {
             
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-			echo "ejecutando la funcion store de centroseducativosModelcentroseducativosCRUD <BR>";
-		
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
-			echo "------------------------- <br> ";
-			echo "NombreCentro ....:". JRequest::getVar( 'NombreCentro') ." <br>";
-			echo "cid... ....:". JRequest::getVar( 'cid') ." <br>";
-			echo "------------------------- <br> ";
-		}
-		
-		if (JRequest::getVar( 'NombreCentro')) {
-                    
-			if (0 == JRequest::getVar( 'cid')) {
-				if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-					echo "ejecutando la funcion store opcion ALTA <BR>";
+            $query = ' SELECT * FROM #__centroseducativos '.
+                            '  WHERE id = '.$this->_id;
+            $this->_db->setQuery( $query );
+            $this->_data = $this->_db->loadObject();
+            
+        }
+        
+        if (!$this->_data) {
+            
+            $this->_data = new stdClass();
+            $this->_data->id = 0;
+            $this->_data->nombre = null;
+            
+        }
+        
+        return $this->_data;
+        
+    }
+        
+    /**
+     * Metodo para almacenar un registro
+     * devuelve una variable booleana con valor true ha finalizado con exito
+     */
+    function store()
+    {
 
-				$query = "INSERT INTO #__centroseducativos (nombre) "
-                   . "VALUES (" . "'". JRequest::getVar( 'NombreCentro') ."') ";
-                                
-			} else {
-				if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-					echo "ejecutando la funcion store opcion UPDATE <BR>";
-				
-                                $query = "UPDATE #__centroseducativos
-							 SET nombre = '" .JRequest::getVar( 'NombreCentro') ."' 
-						   WHERE id=".JRequest::getVar( 'cid') ;				
-			}
-                        
-			if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-				echo "Contenido de la variable query: " . $query . "<br>";
-			$bd = JFactory::getDBO();
-			$bd->setQuery( $query );
-			$saludoBD = $bd->execute();
-			if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG'))
-				echo "Contenido de la variable saludoBD: " . $saludoBD . "<br>";
-                        
-			return $saludoBD;			
-		}	
-		return TRUE;	
-	}
+        $this->mostrarZona(__CLASS__,__METHOD__);
         
-        /**
-	 * Method to delete record(s)
-	 *
-	 * @access	public
-	 * @return	boolean	True on success
-	 */
-	function delete($id)
-	{
-		if (JRequest::getVar( 'DEBUG') == "SI")
-			echo "delete: " . $id . "<br>";	
-		#return true;
-		if ($id) {
-			if (JRequest::getVar( 'DEBUG') == "SI")
-				echo "ejecutando la funcion delete <BR>";
-		
-			$query = "DELETE FROM #__centroseducativos "
-					. "WHERE id=".$id ;
-			if (JRequest::getVar( 'DEBUG') == "SI")
-				echo "Contenido de la variable query: " . $query . "<br>";
-			$bd = JFactory::getDBO();
-			$bd->setQuery( $query );
-			if (!$bd->execute()) {
-				return $bd->getErrorMsg();
-			} else 
-				return "Se ha borrado con EXITO el registro de id: " .$id  ;
-		}
-	}
+        if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
+                echo "------------------------- <br> ";
+                echo "NombreCentro ....:". JRequest::getVar( 'NombreCentro') ." <br>";
+                echo "cid... ....:". JRequest::getVar( 'cid') ." <br>";
+                echo "------------------------- <br> ";
+        }
+
+        if (JRequest::getVar( 'NombreCentro')) {
+
+            if (0 == JRequest::getVar( 'cid')) {
+
+                $query = "INSERT INTO #__centroseducativos (nombre) "
+                . "VALUES (" . "'". JRequest::getVar( 'NombreCentro') ."') ";
+
+            } else {
+
+                $query = "UPDATE #__centroseducativos
+                SET nombre = '" .JRequest::getVar( 'NombreCentro') ."' 
+                WHERE id=".JRequest::getVar( 'cid') ;				
+
+            }
+
+            if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
+                echo "Query: " . $query . "<br>";
+                echo "------------------------- <br> ";
+            }
+
+            $bd = JFactory::getDBO();
+            $bd->setQuery( $query );
+            $centroBD = $bd->execute();
+            if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
+                echo "centroBD: " . $centroBD . "<br>";
+                echo "------------------------- <br> ";
+            }
+
+            return $centroBD;			
+                
+        }	
         
+        return TRUE;	
+        
+    }
+    
+    function delete($id)
+    {
+        
+        $this->mostrarZona(__CLASS__,__METHOD__);
+        
+        if ($id) {
+
+            $query = "DELETE FROM #__centroseducativos "
+                            . "WHERE id=".$id ;
+            
+            if (JRequest::getVar( 'DEBUG') == "SI") {
+                echo "Query: " . $query . "<br>";
+                echo "------------------------- <br> ";
+            }
+                    
+            $bd = JFactory::getDBO();
+            $bd->setQuery( $query );
+            if (!$bd->execute()) {
+                    return $bd->getErrorMsg();
+            } else 
+                    return "Se ha borrado con EXITO el registro de id: " .$id  ;
+        }
+    }
+
+    function mostrarZona($clase,$metodo){
+
+    if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
+            echo "------------------------- <br> ";
+            echo "Clase: ". $clase ." <br>";
+            echo "Método: ". $metodo ." <br>";
+            echo "------------------------- <br> ";
+    }
+
+    }
 }
