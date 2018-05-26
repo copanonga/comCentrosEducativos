07878b4fde48ec2944658e69866601f363dfad73
Jose <josemsg@gmail.com>
2018-05-26 11:29:31 +0200

    Arreglado error de depuración

--- admin/controller.php
+++ admin/controller.php
@@ -1,25 +1,22 @@
 <?php
-/*
- * ********************************************************* 
- * controller.php
- * ********************************************************* 
- */
 
 defined ( '_JEXEC' ) or die ( 'Acceso restringido' );
 
 class centroseducativosController extends JControllerLegacy {
 	
-	public function display($cachable = false, $urlparams = array())
-	{
+    public function display($cachable = false, $urlparams = array())
+    {
 
-		if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
-			echo "------------------------- <br> ";		
-			echo "estoy en .....:". __CLASS__." <br>";
-			echo "estoy en .....:". __METHOD__." <br>";
-			echo "------------------------- <br> ";
-			echo "ejecutando la funcion display de centroseducativosController <BR>";
-		}
-		parent::display();
-	}
+        if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
+            echo "------------------------- <br> ";
+            echo "Clase: ". __CLASS__ ." <br>";
+            echo "Método: ". __METHOD__ ." <br>";
+            echo "------------------------- <br> ";
+        }
+
+        parent::display();
+
+    }
+    
 }
 
--- admin/views/centroseducativoscrud/tmpl/default.php
+++ admin/views/centroseducativoscrud/tmpl/default.php
@@ -11,8 +11,8 @@ defined ( '_JEXEC' ) or die ( 'Acceso restringido' );
 
 if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
     echo "------------------------- <br> ";
-    echo "Clase: ". $clase ." <br>";
-    echo "Método: ". $metodo ." <br>";
+    echo "Clase: ". __CLASS__ ." <br>";
+    echo "Método: ". __METHOD__ ." <br>";
     echo "------------------------- <br> ";
 }
         
